f00fbdecc0db5adc1f02c9de13246568da79bf86
juvariashahid <juvariashahid3@gmail.com>
2020-12-26 12:49:27 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # -- Project information -----------------------------------------------------
 
 project = 'MWB'
@@ -25,9 +27,6 @@ author = 'Juvaria Shahid'
 release = '0.1.0'
 master_doc = "index"
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # -- General configuration ---------------------------------------------------
